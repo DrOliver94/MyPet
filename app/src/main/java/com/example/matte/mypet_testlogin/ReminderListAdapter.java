ea094dc223cd16ece1777b14202039b96ec68c57
Matteo Oliveri <matteo.oliveri.oliver@gmail.com>
2016-11-19 18:43:19 +0100

    Added icons. Created RemidnerListAdapter

---
+++ app/src/main/java/com/example/matte/mypet_testlogin/ReminderListAdapter.java
@@ -0,0 +1,55 @@
+package com.example.matte.mypet_testlogin;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import java.util.ArrayList;
+
+/**
+ * Created by matte on 161031.
+ */
+
+public class ReminderListAdapter extends BaseAdapter {
+
+    private Context context;
+    private ArrayList<Post> posts;
+
+    public ReminderListAdapter(Context context, ArrayList<Post> posts){
+        this.context = context;
+        this.posts = posts;
+    }
+
+    @Override
+    public int getCount() {
+        return posts.size();
+    }
+
+    @Override
+    public Object getItem(int pos) {
+        return posts.get(pos);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int pos, View view, ViewGroup viewGroup) {
+        PostLayout postLayout = null;
+        Post p = posts.get(pos);
+
+//        if(view == null){
+        if(true){
+            postLayout = new PostLayout(context, p);
+        } else {
+            postLayout = (PostLayout) view;
+            postLayout.setPost(p);
+        }
+
+
+        return postLayout;
+    }
+}
