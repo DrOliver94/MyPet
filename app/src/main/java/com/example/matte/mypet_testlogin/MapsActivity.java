e0fb7b7a59d922da17071673e532e01e981309e8
Silvia Lombardo <silvial1993@gmail.com>
2016-11-23 12:10:00 +0100

    Sistemate mappe e api bzzzzz

--- app/src/main/java/com/example/matte/mypet_testlogin/MapsActivity.java
+++ app/src/main/java/com/example/matte/mypet_testlogin/MapsActivity.java
@@ -77,12 +77,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-
-
-
         // Add a marker in Sydney and move the camera
 //        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(latlng).title("Marker in Sydney"));
+        mMap.addMarker(new MarkerOptions().position(latlng));
 //        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latlng, 16)); //1 - molto lontano; ~20 molto vicino
 
